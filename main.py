f424097b0d1f9031ce9d61a7066e48d338cb1b19
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-06 10:49:41 -0300

    chore: Comentários nos códigos de login e de registro.

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ while not esta_logado:
         #Variável de controle do while de registro
         salvo = False
 
+        #WHILE DE REGISTRO - SUPER IMPORTANTE!!!
         while not salvo:
             email_registro = input("Insira o seu email: ")
             senha_registro = input("Insira a sua senha: ")
@@ -75,16 +76,21 @@ while not esta_logado:
     ):
         print("Certo, efetuaremos a sua autenticação a seguir. \n")
 
+        #Variável de controle do while de login
         conseguiu_logar = False
 
+        #WHILE DE LOGIN - SUPER IMPORTANTE!!!
         while not conseguiu_logar:
             email_login = input("Insira o seu email: ")
             senha_login = input("Insira a sua senha: ")
 
+            #Se o tamanho da lista de contas for 0, a conta não existe (nenhuma existe)
             if len(lista_contas_pacientes) == 0:
                 print("Conta inexistente!")
 
+            #Itera sobre cada conta na lista de contas
             for conta in lista_contas_pacientes:
+                #Checa se o email e a senha da conta da iteração atual coincidem com as informações informadas pelo usuário
                 if conta.get("email") == email_login and conta.get("senha") == senha_login:
                     print("Login realizado com sucesso! \n\n")
 
@@ -93,11 +99,13 @@ while not esta_logado:
                     email_atual_logado = email_login
                     break
 
+                #Checa se o email coincide, mas a senha não.
+                    #Isso significa que a conta existe, mas a senha está errada. Fala pro usuário e reseta o loop
                 elif conta.get("email") == email_login and conta.get("senha") != senha_login:
                     print("Senha incorreta! \n Tente novamente.")
-
                     break
 
+                #Se o email não coincidiu com alguma conta, ela não existe.
                 else:
                     print("Conta inexistente!")
 
